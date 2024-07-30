f3ea8c93e91315a6a15d480582a5f0532dacef17
Jesus Bernat <contact@asedem.com>
2024-07-30 10:31:43 +0200

    Ejercicio completo

--- main.cpp
+++ main.cpp
@@ -3,17 +3,37 @@
 using namespace std;
 
 int main() {
-  // Create and open a text file
-  fstream MyFile("filename.txt");
-
-  // Write to the file
-  MyFile << "Files can be tricky, but it is fun enough!";
+  ifstream fichero("listadoAlumnos.txt");
+  int accion;
+  string nombre;
+  string linea;
+  string contenido = "";
   
-  // Read from the file
-  string myText;
-  getline(MyFile, myText);
-  cout << myText;
+  cout << "LISTADO DE ALUMNOS: \n";
+  while ( getline(fichero, linea) ) {
+    cout << linea + "\n";
+    contenido += linea + "\n";
+  }
+
+  cout << "- Quieres poner un nombre nuevo? \n";
+  cout << "- 1. Nombre nuevo\n";
+  cout << "- 2. Salir\n";
+  cin >> accion;
+  switch(accion){
+    case 1:
+      cout << "Nuevo nombre: ";
+      cin >> nombre;
+      contenido += nombre + "\n";
+      break;
+    case 2: exit(0);
+    default: exit(0);
+  }
 
   // Close the file
-  MyFile.close();
+  fichero.close();
+
+
+  ofstream f("listadoAlumnos.txt");
+  f << contenido;
+  f.close();
 } 
